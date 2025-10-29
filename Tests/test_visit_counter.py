ab50577f3f7e46c0b696fa25ce3a48e63e3340aa
Fasi Mohammed <fasifaiz799@gmail.com>
2025-10-28 21:19:05 -0300

    added mock table import

--- Tests/test_visit_counter.py
+++ Tests/test_visit_counter.py
@@ -3,6 +3,8 @@ import os
 import json
 import pytest
 import azure.functions as func
+from unittest.mock import patch, MagicMock
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from function_app import VisitCounter
